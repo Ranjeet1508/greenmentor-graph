a34a2237b9b8f0935ce1a4ebf971ce1440f2d37c
Ranjeet1508 <danavrj1998@gmail.com>
2024-01-11 09:45:27 +0530

    added responsiveness

--- src/Components/Navbar/Timeline.jsx
+++ src/Components/Navbar/Timeline.jsx
@@ -5,7 +5,9 @@ const Timeline = () => {
   return (
     <div className='timeline'>
       <select>
-        <option value="Jan20-Dec21">Show Timeline: <span className='date'>Jan'22-Dec'23</span></option>
+        <option value="Jan22-Dec23">Show Timeline: <span className='date'>Jan'22-Dec'23</span></option>
+        <option value="Apr22-Apr23">Show Timeline: <span className='date'>Apr'22-Apr'23</span></option>
+        <option value="Aug22-Aug23">Show Timeline: <span className='date'>Aug'22-Aug'23</span></option>
       </select>
     </div>
   )
